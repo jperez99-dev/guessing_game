c231765c156089858f139505d0f72568259230f5
Jordan Perez <jordan.perez01@gmail.com>
2022-07-10 14:59:30 -0500

    Just a simple guessing game.

---
+++ guessing_game.py
@@ -0,0 +1,33 @@
+#This is a simple guessing game
+import random
+
+def guessNumber():
+    randomNumber = random.randrange(1,10)
+    answer = False
+    count = 0
+    while answer == False:
+        
+        try:
+            guess = int(input("Please choose a number 1 - 10 "))
+            if guess < 1 or guess > 10:  
+                raise ValueError
+        except ValueError as err:
+           print(f"Error: Please enter a numeber 1-10. {err} ")
+        else:
+            if guess == randomNumber:
+                
+                print("Congrats you guessed right!")
+                print(f"It took you {count} tries")
+                break
+            elif guess < randomNumber:
+                    print("It's higher")
+            else: 
+                print("It's lower")
+        count+=1
+
+def start_game():
+    print("Welcome to my guessing game!")
+    guessNumber()
+
+
+start_game()
